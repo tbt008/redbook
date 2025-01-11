bddba702394bc5f49e61682b55e1eb25880f5fde
qibin wang <480256233@qq.com>
2025-01-11 16:38:47 +0800

    第二次合并加完善

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,16 @@
 <script setup lang="ts">
 import Header from '@/components/OJHeader.vue'
+import { useRoute } from 'vue-router'
+
+const route = useRoute()
+const hideHeaderPaths = ['/ai'] // 在这里添加不需要显示导航栏的路径
 </script>
+
 <template>
   <div id="main">
-    <Header></Header>
+    <Header v-if="!hideHeaderPaths.includes(route.path)"></Header>
     <router-view></router-view>
   </div>
 </template>
+
 <style scoped></style>
