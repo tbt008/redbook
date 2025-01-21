be0bd49a80ef3ed65410bf8f0f237b36bc1e23ed
qibin wang <480256233@qq.com>
2025-01-22 04:19:16 +0800

    修复跳转会到注册页（a标签会刷新浏览器，用router-link解决

--- src/views/register.vue
+++ src/views/register.vue
@@ -1,20 +1,11 @@
 <template>
   <section class="container">
     <div class="svg-top">
-      <svg
-        version="1.1"
-        xmlns="http://www.w3.org/2000/svg"
-        xmlns:xlink="http://www.w3.org/1999/xlink"
-        height="1337"
-        width="1337"
-      >
+      <svg version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" height="1337"
+        width="1337">
         <defs>
-          <path
-            id="path-1"
-            opacity="1"
-            fill-rule="evenodd"
-            d="M1337,668.5 C1337,1037.455193874239 1037.455193874239,1337 668.5,1337 C523.6725684305388,1337 337,1236 370.50000000000006,1094 C434.03835568300906,824.6732385973953 6.906089672974592e-14,892.6277623047779 0,668.5000000000001 C0,299.5448061257611 299.5448061257609,1.1368683772161603e-13 668.4999999999999,0 C1037.455193874239,0 1337,299.544806125761 1337,668.5Z"
-          />
+          <path id="path-1" opacity="1" fill-rule="evenodd"
+            d="M1337,668.5 C1337,1037.455193874239 1037.455193874239,1337 668.5,1337 C523.6725684305388,1337 337,1236 370.50000000000006,1094 C434.03835568300906,824.6732385973953 6.906089672974592e-14,892.6277623047779 0,668.5000000000001 C0,299.5448061257611 299.5448061257609,1.1368683772161603e-13 668.4999999999999,0 C1037.455193874239,0 1337,299.544806125761 1337,668.5Z" />
           <linearGradient id="linearGradient-2" x1="0.79" y1="0.62" x2="0.21" y2="0.86">
             <stop offset="0" stop-color="rgb(88,62,213)" stop-opacity="1" />
             <stop offset="1" stop-color="rgb(23,215,250)" stop-opacity="1" />
@@ -26,20 +17,11 @@
       </svg>
     </div>
     <div class="svg-bottom">
-      <svg
-        version="1.1"
-        xmlns="http://www.w3.org/2000/svg"
-        xmlns:xlink="http://www.w3.org/1999/xlink"
-        height="896"
-        width="967.8852157128662"
-      >
+      <svg version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" height="896"
+        width="967.8852157128662">
         <defs>
-          <path
-            id="path-2"
-            opacity="1"
-            fill-rule="evenodd"
-            d="M896,448 C1142.6325445712241,465.5747656464056 695.2579309733121,896 448,896 C200.74206902668806,896 5.684341886080802e-14,695.2579309733121 0,448.0000000000001 C0,200.74206902668806 200.74206902668791,5.684341886080802e-14 447.99999999999994,0 C695.2579309733121,0 475,418 896,448Z"
-          />
+          <path id="path-2" opacity="1" fill-rule="evenodd"
+            d="M896,448 C1142.6325445712241,465.5747656464056 695.2579309733121,896 448,896 C200.74206902668806,896 5.684341886080802e-14,695.2579309733121 0,448.0000000000001 C0,200.74206902668806 200.74206902668791,5.684341886080802e-14 447.99999999999994,0 C695.2579309733121,0 475,418 896,448Z" />
           <linearGradient id="linearGradient-3" x1="0.5" y1="0" x2="0.5" y2="1">
             <stop offset="0" stop-color="rgb(40,175,240)" stop-opacity="1" />
             <stop offset="1" stop-color="rgb(18,15,196)" stop-opacity="1" />
@@ -63,20 +45,15 @@
           <input v-model="username" type="text" placeholder="学号" />
           <input v-model="studentId" type="text" placeholder="用户姓名" />
           <el-select v-model="classic" class="classic-select" placeholder="请选择班级">
-            <el-option
-              v-for="item in classicList"
-              :key="item.id"
-              :label="item.className"
-              :value="item.id"
-            />
+            <el-option v-for="item in classicList" :key="item.id" :label="item.className" :value="item.id" />
           </el-select>
           <input v-model="password" type="password" placeholder="密码" />
-          <input v-model="confirmPassword" type="password" placeholder="确认密码" /> 
+          <input v-model="confirmPassword" type="password" placeholder="确认密码" />
           <input type="button" class="login-button" value="注册" @click="register" />
         </form>
       </section>
       <footer>
-        <p><a href="/login" title="Login">已有账号？立即登录</a></p>
+        <p><router-link to="/login" title="Login">已有账号？立即登录</router-link></p>
       </footer>
     </section>
   </section>
@@ -94,7 +71,7 @@ const username = ref('')
 const studentId = ref('')
 const classic = ref('')
 const password = ref('')
-const confirmPassword = ref('') 
+const confirmPassword = ref('')
 
 const classicList = ref([])
 
@@ -118,24 +95,24 @@ onMounted(() => {
 
 const register = async () => {
   // 表单验证
-  if (!username.value || !password.value || !confirmPassword.value || !classic.value||!studentId.value) {
+  if (!username.value || !password.value || !confirmPassword.value || !classic.value || !studentId.value) {
     ElMessage.warning('请填写所有必填项')
     return
   }
-  
+
   if (password.value !== confirmPassword.value) {
     ElMessage.error('两次输入的密码不一致')
     return
   }
-  
+
   const registerData = {
     uid: username.value,
-    password: password.value, 
+    password: password.value,
     classic: classic.value,
     studentId: studentId.value
   }
 
-  try { 
+  try {
     const res = await request.post('/user/register', registerData)
     if (res.code === 200) {
       ElMessage.success('注册成功！')
@@ -153,6 +130,7 @@ const register = async () => {
 input {
   border-bottom: 1px solid var(--el-border-color);
 }
+
 .container {
   position: absolute;
   display: flex;
@@ -162,16 +140,19 @@ input {
   overflow: hidden;
   margin: 0 0 0 0;
   width: 100%;
+
   .svg-top {
     position: absolute;
     top: -900px;
     right: -300px;
   }
+
   .svg-bottom {
     position: absolute;
     bottom: -500px;
     left: -200px;
   }
+
   .wrapper {
     padding: 40px;
     background-color: #fff;
@@ -179,8 +160,10 @@ input {
     width: 350px;
     border: 1px solid rgb(36, 184, 242);
     z-index: 1;
+
     header {
       margin-bottom: 40px;
+
       .logo {
         width: 70px;
         height: 70px;
@@ -189,11 +172,13 @@ input {
         display: flex;
         justify-content: center;
         align-items: center;
+
         span {
           font-size: 18px;
           color: #fff;
         }
       }
+
       h1 {
         color: #6065d9;
         font-size: 35px;
@@ -201,6 +186,7 @@ input {
         margin-bottom: 0;
         margin-top: 40px;
       }
+
       p {
         color: #6065d9;
         font-size: 18px;
@@ -208,6 +194,7 @@ input {
         margin: 5px 0 0 0;
       }
     }
+
     .main-content {
       input {
         border: none;
@@ -217,20 +204,24 @@ input {
         margin: 15px 0;
         font-size: 18px;
         color: #999;
+
         &::placeholder {
           color: #999;
           font-size: 18px;
           font-weight: 300;
         }
+
         &:focus {
           outline: none;
         }
       }
+
       .line {
         width: 100%;
         height: 2px;
         background-color: #99999955;
       }
+
       .login-button {
         background: linear-gradient(to right, #6065d9, #17d7fa);
         border: none;
@@ -246,14 +237,17 @@ input {
         cursor: pointer;
       }
     }
+
     footer {
       display: flex;
       justify-content: center;
       align-items: flex-end;
       margin-top: 60px;
+
       p {
         margin: 0;
         font-weight: 100;
+
         a {
           color: #6065d9;
           text-decoration: none;
@@ -267,6 +261,7 @@ input {
   .wrapper {
     margin: 0 10px !important;
     padding: 30px;
+
     header {
       h1 {
         font-size: 30px;
@@ -279,4 +274,4 @@ input {
   width: 100%;
   margin: 15px 0;
 }
-</style> 
+</style>
